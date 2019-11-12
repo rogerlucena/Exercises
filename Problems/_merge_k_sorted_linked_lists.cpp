2379cda7cca2cf354ce12e64a148bf89258f4a0b
Roger Leite Lucena <rogerleitelucena@gmail.com>
2019-11-12 16:41:10 +0100

    Add merge_k_sorted_linked_lists - heap, linked lists

---
+++ Problems/_merge_k_sorted_linked_lists.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#include "tools.cpp"
+
+using namespace std;
+
+// https://www.interviewbit.com/problems/merge-k-sorted-lists/
+
+// Merge k sorted linked lists and return it as one sorted list.
+
+// Example:
+// 1 -> 10 -> 20
+// 4 -> 11 -> 13
+// 3 -> 8 -> 9
+// will result in
+// 1 -> 3 -> 4 -> 8 -> 9 -> 10 -> 11 -> 13 -> 20
+
+ListNode* mergeKLists(vector<ListNode*> &A) {
+	auto cmp = [](const ListNode* a, const ListNode* b) -> bool {return a->val > b->val;};
+	priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> q(cmp, A);
+	
+	ListNode ans(-1);
+	ListNode* tmp = &ans;
+
+	while(!q.empty()) {
+		ListNode* next = q.top();
+		q.pop();
+
+		tmp->next = next;
+		next = next->next;
+		if(next) {
+			q.push(next);
+		}
+		tmp = tmp->next;
+	}
+
+	return ans.next;
+}
+
+int main() {
+	ListNode* l1 = generateList({1, 10, 20});
+	ListNode* l2 = generateList({4, 11, 13});
+	ListNode* l3 = generateList({3, 8, 9});
+
+	vector<ListNode*> A = {l1, l2, l3};
+	printList(mergeKLists(A));
+
+	return 0;
+}
--- remember_priority_queue.cpp
+++ remember_priority_queue.cpp
@@ -12,6 +12,10 @@
 
 // defaut comparator is smaller (as smaller -> as weak then, less priority), pop will give the greatest number
 
+// General syntax:
+// auto cmp = [](int left, int right) {return left > right;};
+// priority_queue<int, vector<int>, decltype(cmp)> q3(cmp, container);
+
 template<typename T> void print_queue(T& q) {
     while(!q.empty()) {
         std::cout << q.top() << " ";
@@ -21,28 +25,30 @@ template<typename T> void print_queue(T& q) {
 }
 
 int main() {
-    std::priority_queue<int> q;
+    std::priority_queue<int> q1;
     // priority_queue<int> pq (A.begin(), A.end()); // Initilize O(n) taking a container as input
 
     for(int n : {1,8,5,6,3,4,0,9,7,2})
-        q.push(n);
+        q1.push(n);
 
-    print_queue(q); // defaut is "smaller" comparator, great numbers have better priority then
+    std::cout << "q1: ";
+    print_queue(q1); // defaut is "smaller" comparator, great numbers have better priority then
 
     std::priority_queue<int, std::vector<int>, std::greater<int> > q2;
 
     for(int n : {1,8,5,6,3,4,0,9,7,2})
         q2.push(n);
 
+    std::cout << "q2: ";
     print_queue(q2);
 
+    // Good way of initializing, using lambda and vector as arguments
     // Using lambda to compare elements.
-    auto cmp = [](int left, int right) { return (left ^ 1) < (right ^ 1);}; // XOR (exclusive or)
-    std::priority_queue<int, std::vector<int>, decltype(cmp)> q3(cmp);
+    // auto cmp = [](int left, int right) { return (left ^ 1) < (right ^ 1);}; // XOR (exclusive or)
+    auto cmp = [](int left, int right) {return left > right;};
+    std::priority_queue<int, std::vector<int>, decltype(cmp)> q3(cmp, {1,8,5,6,3,4,0,9,7,2});
 
-    for(int n : {1,8,5,6,3,4,0,9,7,2})
-        q3.push(n);
-
+    std::cout << "q3: ";
     print_queue(q3);
 
     int arr[] = {1, 3, 2, 6, 6, 5};
