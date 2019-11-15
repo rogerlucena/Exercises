9c45569224f39ba370d6334d38a3e60cb04fb26d
Roger Leite Lucena <rogerleitelucena@gmail.com>
2019-11-15 14:28:36 +0100

    Add course_schedule_dependecies problem (interesting: topological sorting, graphs) and more notes as well

---
+++ Problems/__course_schedule_dependecies.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+// https://leetcode.com/problems/course-schedule/
+// (topological sort is usually used when dealing with scheduling with dependencies, like here for example)
+
+// There are a total of n courses you have to take, labeled from 0 to n-1.
+// Some courses may have prerequisites, for example to take course 0 you have to first take course 1, 
+// which is expressed as a pair: [0,1]
+// Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+// Example 1:
+// Input: 2, [[1,0]] 
+// Output: true
+// Explanation: There are a total of 2 courses to take. 
+//              To take course 1 you should have finished course 0. So it is possible.
+
+// Example 2:
+// Input: 2, [[1,0],[0,1]]
+// Output: false
+// Explanation: There are a total of 2 courses to take. 
+//              To take course 1 you should have finished course 0, and to take course 0 you should
+//              also have finished course 1. So it is impossible.
+
+// Note: You may assume that there are no duplicate edges in the input prerequisites.
+
+bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+	// Edge v -> u if u depend on v (with TopoSort all the dependents will be to the right then - that is precisely what we want)
+    vector<vector<int>> g(numCourses, vector<int>());
+	vector<int> inDegree(numCourses, 0);
+	for(vector<int> e : prerequisites) {
+		g[e[1]].push_back(e[0]);
+		++inDegree[e[0]];
+	}
+
+	// Property invariant for "q": a vertex only enters "q" if its current inDegree is 0 
+	// (never happens for nodes in a cycle)  
+	queue<int> q;
+	for(int i = 0; i < numCourses; ++i) {
+		if(inDegree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	int visitedCount = 0;
+	while(!q.empty()) {
+		// If that vertex has current inDegree == 0, then it can now be pushed back into 
+		// our virtual Topological Ordering without breaking any dependencies
+		++visitedCount;
+		int v = q.front();
+		q.pop();
+
+		for(int neighbor : g[v]) {
+			 if(--inDegree[neighbor] == 0) {
+				 q.push(neighbor);
+			 }
+		 }
+	}
+	
+	return visitedCount == numCourses;
+}
+
+// Topological sort with DFS (work if it is a DAG, but does not verify if it is indeed a graph)
+void topologicalOrderingDFS(const vector<vector<int>> &g, int v, vector<bool> &visited, stack<int> &topoOrdering) {
+	visited[v] = true;
+	for(int neighbor : g[v]) {
+		if(!visited[neighbor]) {
+			topologicalOrderingDFS(g, neighbor, visited, topoOrdering);
+		}
+	}
+
+	topoOrdering.push(v);
+}
+
+stack<int> possibleSchedulingOrder(int numCourses, vector<vector<int>> &prerequisites) {
+    vector<vector<int>> g (numCourses, vector<int>());
+	for(vector<int> e : prerequisites) {
+		g[e[1]].push_back(e[0]);
+	}
+
+	vector<bool> visited (numCourses, false);
+	stack<int> topoOrdering;
+	for(int v = 0; v < numCourses; ++v) {
+		if(!visited[v]) {
+			topologicalOrderingDFS(g, v, visited, topoOrdering);
+		}
+	}
+
+	return topoOrdering;
+}
+
+int main() {
+	int numCourses = 2;
+	// vector<vector<int>> prerequisites = {{1, 0}}; // ans: 1
+	vector<vector<int>> prerequisites = {{1, 0}, {0, 1}}; // ans: 0
+
+	cout << "Answer: " << canFinish(numCourses, prerequisites) << endl;
+
+	return 0;
+}
--- main_2.cpp
+++ main_2.cpp
@@ -7,3 +7,70 @@ int main() {
 	cout << "Hello, World!";
 	return 0;
 }
+
+
+
+
+
+
+
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int isMatchHelper(const string &A, int ia, const string &B, int ib, vector<vector<int>> &m) {
+// 	if(m[ia][ib] != -1) {
+// 		return m[ia][ib];
+// 	}
+	
+// 	if(ia < A.size() && A[ia] == '*') {
+// 		// int ans = 0;
+// 		// for(int i = 0; !ans && (i == 0 || ib+i <= B.size()); ++i) {// || (ib+i < B.size() && B[ib+i] == B[ib+i-1])); ++i) {
+// 		// 	ans = ans || isMatchHelper(A, ia+1, B, ib+i, m);
+// 		// }
+// 		m[ia][ib] = (ib < B.size() && isMatchHelper(A, ia, B, ib+1, m)) || isMatchHelper(A, ia+1, B, ib, m);
+// 	} else if(ib < B.size() && B[ib] == '*') {
+// 		// int ans = 0;
+// 		// for(int i = 0; !ans && (i == 0 || ia+i <= A.size()); ++i) { // || (ia+i < A.size() && A[ia+i] == A[ia+i-1])); ++i) {
+// 		// 	ans = ans || isMatchHelper(A, ia+i, B, ib+1, m);
+// 		// }
+// 		m[ia][ib] = (ia < A.size() && isMatchHelper(A, ia+1, B, ib, m)) || isMatchHelper(A, ia, B, ib+1, m);
+// 	} else if(ia == A.size() || ib == B.size()) {
+// 		m[ia][ib] = ia == A.size() && ib == B.size();
+// 	} else if(A[ia] == '?' || B[ib] == '?' || A[ia] == B[ib]) {
+// 		m[ia][ib] = isMatchHelper(A, ia+1, B, ib+1, m);
+// 	} else {
+// 		m[ia][ib] = false;
+// 	}
+
+// 	return m[ia][ib];
+// }
+
+// int isMatch(const string A, const string B) {
+// 	vector<vector<int>> m (A.size()+1, vector<int>(B.size()+1, -1));
+// 	return isMatchHelper(A, 0, B, 0, m);
+// }
+
+
+// int main() {
+// 	vector<string> A = {"aa", "aa", "aaa", "aa", "aa", "ab", "aab"};
+//     vector<string> B = {"a",  "aa", "aa",  "*",  "a*", "?*", "c*a*b"};
+// 	vector<int>  res = {0,    1,    0,     1,    1,     1,   0};
+	
+// 	int i = 0;
+// 	for(; i < A.size(); ++i) {
+// 		if(isMatch(A[i], B[i]) != res[i]) {
+// 			cout << "Problem in case: " << i << endl;
+// 			break;
+// 		}
+// 	}
+
+// 	if(i == A.size()) {
+// 		cout << "Program passed all the tests" << endl;
+// 	}
+
+
+// 	return 0;
+// }
