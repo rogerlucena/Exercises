084012fec4ac04def5f8c315f5b9fb979ac9fd25
Roger Leite Lucena <rogerleitelucena@gmail.com>
2019-11-23 16:01:38 +0100

    Add clone_graph problem - DFS and hash map

---
+++ Problems/clone_graph.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+// https://www.interviewbit.com/problems/clone-graph/
+// DFS over original and hash map saving original -> clone
+
+// Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+// Definition for undirected graph.
+struct UndirectedGraphNode {
+	int label;
+	vector<UndirectedGraphNode*> neighbors;
+	UndirectedGraphNode(int x) : label(x) {};
+};
+
+UndirectedGraphNode* cloneGraphHelper(UndirectedGraphNode* node, unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> &orginalAndClone) {
+	if(!node) {
+		return nullptr;
+	}
+
+	UndirectedGraphNode* newNode = new UndirectedGraphNode(node->label);
+	orginalAndClone[node] = newNode;
+
+	for(UndirectedGraphNode* neigh : node->neighbors) {
+		if(orginalAndClone.find(neigh) == orginalAndClone.end()) {
+			newNode->neighbors.push_back(cloneGraphHelper(neigh, orginalAndClone));
+		} else {
+			newNode->neighbors.push_back(orginalAndClone[neigh]);
+		}
+	}
+
+	return orginalAndClone[node];
+}
+
+UndirectedGraphNode* cloneGraph(UndirectedGraphNode* node) {
+	unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> orginalAndClone;
+	return cloneGraphHelper(node, orginalAndClone);
+}
+
+int main() {
+	cout << "Hello, World!";
+	return 0;
+}
