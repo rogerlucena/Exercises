8b53df52084dcf0f7717d0d73bf429b008b31767
Roger Leite Lucena <rogerleitelucena@gmail.com>
2020-04-05 12:08:42 -0300

    Update file

--- Problems/__best_time_to_buy_and_sell_stocks.cpp
+++ Problems/__best_time_to_buy_and_sell_stocks.cpp
@@ -54,6 +54,34 @@ int maxProfit(const vector<int> &A) {
 }
 
 
+// Version II of the problem:
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+// Say you have an array for which the ith element is the price of a given stock on day i.
+
+// Design an algorithm to find the maximum profit. You may complete as many transactions 
+// as you like (i.e., buy one and sell one share of the stock multiple times).
+
+// Note: You may not engage in multiple transactions at the same time (i.e., you must sell the 
+// stock before you buy again).
+
+int maxProfit(vector<int> &prices) {
+	if(prices.empty()) {
+		return 0;
+	}
+
+	int s = prices.size();
+    vector<vector<int>> memo(2, vector<int>(s, 0));
+	memo[1][s-1] = prices[s-1];
+	for(int j = s-2; j >= 0; --j) {
+		memo[0][j] = max(memo[0][j+1], memo[1][j+1]-prices[j]);
+		memo[1][j] = max(memo[1][j+1], memo[0][j+1]+prices[j]);
+	}
+
+	return memo[0][0];
+}
+// You can even improve the memory (you only need the 2 states in the future to compute each past state)
+
 int main() {
 	// vector<int> A = {1, 2}; // ans: 1
 	vector<int> A = {1, 4, 5, 2, 4}; // ans: 4
