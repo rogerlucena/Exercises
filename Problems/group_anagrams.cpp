1962121b7611130190d0734e52694fc93d1a974d
Roger Leite Lucena <rogerleitelucena@gmail.com>
2020-04-06 13:08:10 -0300

    Update groupAnagrams problem

--- Problems/group_anagrams.cpp
+++ Problems/group_anagrams.cpp
@@ -3,68 +3,72 @@
 #include <unordered_map>
 #include <sstream>
 
-/*
+#include "tools.cpp"
 
- -- Problem Statement: https://leetcode.com/problems/group-anagrams/description/
- Basically, from a vector of strings, group all the anagrams together
+// https://leetcode.com/problems/group-anagrams/description/
 
-*/
+// From a vector of strings, group all the anagrams together.
+
+// Example:
+
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+
+// Output:
+// [
+// ["ate","eat","tea"],
+// ["nat","tan"],
+// ["bat"]
+// ]
 
 using namespace std;
 
-string genUniqueString(string s) {
-    int occ [26] = {0}; // Fill with zeros
-    string sol;
+string genSignature(string s) {
+    vector<int> occ(26, 0);
+    string signature;
     for(char c : s) {
         occ[c-'a']++;
     }
-    for(int i=0; i<26; ++i){
-        if(occ[i]>0) {
-            sol.push_back('a'+i); // ASCII
-            ostringstream aux; aux << occ[i];
-            sol.append(aux.str());
+    for(int i = 0; i < 26; ++i) {
+        if(occ[i] > 0) {
+            signature.push_back('a' + i); // ASCII
+            ostringstream aux; 
+			aux << occ[i];
+            signature.append(aux.str());
         }
     }
-    return sol;
+
+    return signature;
 }
 
 // time: O(n*m), space: O(n*m)
-vector<vector<string>> groupAnagrams (vector<string> l) {
+vector<vector<string>> groupAnagrams(vector<string> &strs) {
     unordered_map<string, vector<string>> map;
-    vector<string> labels;
     vector<vector<string>> solution;
 
-    for(string s : l) {
-        string unique = genUniqueString(s);
-        if(map.find(unique) != map.end()){
-            map[unique].push_back(s);
-        }
-        else{
-            vector<string> vString;
-            vString.push_back(s);
-            map[unique] = vString;
-            labels.push_back(unique);
+    for(string s : strs) {
+        string signature = genSignature(s);
+
+        if(map.find(signature) != map.end()) {
+            map[signature].push_back(s);
+        } else {
+            map[signature] = {s};
         }
     }
 
-    for(string u : labels){
-        solution.push_back(map.find(u)->second);
+    for(pair<string, vector<string>> p : map) {
+        solution.push_back(p.second);
     }
 
     return solution;
 }
 
+int main() {
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    vector<vector<string>> solution = groupAnagrams(strs);
 
-int main()
-{
-    vector<string> l = {"word", "rdow", "second", "ab", "ba", "tuua", "utua", "autu"};
-    vector<vector<string>> solution = groupAnagrams(l);
-
-    for(vector<string> vS : solution) {
-        for(string s: vS) {
-            cout << "\"" << s << "\"" << " ";
-        }
-        cout << endl;
+    for(const vector<string> vectorOfAnagrams : solution) {
+		function<void(const string&)> printEl = [](const string &s) -> void {cout << s;};
+        printVector(vectorOfAnagrams, printEl);
     }
 
     return 0;
