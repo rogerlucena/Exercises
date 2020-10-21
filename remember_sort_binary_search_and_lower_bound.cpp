5a5401e142803def01101319e04858d8a723e355
Roger Leite Lucena <rogerleitelucena@gmail.com>
2020-10-20 22:56:12 -0300

    Add more information about lower_bound and upper_bound to the remember_sort_binary_search_and_lower_bound file

--- remember_sort_binary_search_and_lower_bound.cpp
+++ remember_sort_binary_search_and_lower_bound.cpp
@@ -13,15 +13,38 @@ auto myfunction = [](int i,int j) { return (i>j); };
 // "-> bool" not necessary in lambda function
 
 // Lower bound remember (in <algorithm>) - http://www.cplusplus.com/reference/algorithm/lower_bound/:
-// Returns an iterator pointing to the first element in the range [first,last) which does not compare less than val (>= val then).
-// Unlike upper_bound, the value pointed by the iterator returned by this function may also be equivalent to val, 
-// and not only greater.
-// (useful when wanting the position of binary_search)
+// Returns an iterator pointing to the first element in the range [first,last) which does not compare less 
+// than val (>= val then, if multiple returns iterator to 1st one; iterator to last if not found).
+// Unlike upper_bound (first in [first, last) that is > val, iterator to last if not found), the value 
+// pointed by the iterator returned by lower_bound function may also be equivalent to val, and not only 
+// greater (useful when wanting the position of binary_search).
 // Note: the array must be sorted with comp, default is "<"
 // template <class ForwardIterator, class T, class Compare>
 //   ForwardIterator lower_bound (ForwardIterator first, ForwardIterator last, const T& val, Compare comp);
 
-int main () {
+// P.S.: upper_bound and lower_bound work the same way maps and sets too:
+// but remember, for maps and sets we cannot do operations with the iterators such 
+// as "m.upper_bound(n) - m.begin()", or "m.begin()+1" (bidirectional iterators do not allow it), only ++ or --
+
+int rememberLowerBound() {
+    vector<int> v{10, 20, 30, 30, 30, 40, 50}; 
+    vector<int>::iterator low1, low2, low3; 
+      
+    // std :: lower_bound 
+    low1 = lower_bound(v.begin(), v.end(), 30); 
+    low2 = lower_bound(v.begin(), v.end(), 35); 
+    low3 = lower_bound(v.begin(), v.end(), 55); 
+  
+    // Printing the lower bounds 
+    cout << "lower_bound for element 30 at position : " << (low1 - v.begin()); // 2
+    cout << "\nlower_bound for element 35 at position : " << (low2 - v.begin()); // 5
+    cout << "\nlower_bound for element 55 at position : " << (low3 - v.begin()); // 7
+    
+    return 0;
+}
+
+
+int rememberSortAndBinarySearch() {
     int myints[] = {1,2,3,4,5,4,3,2,1};
     std::vector<int> v(myints,myints+9);  // 1 2 3 4 5 4 3 2 1
 
@@ -50,6 +73,7 @@ int main () {
 
 // looking for a 3... found!
 // looking for a 6... not found.
+
 
 
 // Binary Search - implementation:
