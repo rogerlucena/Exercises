a97121434b86e60191ba8318fc77e85c2c1c54c8
Roger Leite Lucena <rogerleitelucena@gmail.com>
2022-06-06 15:55:33 +0200

    Add hare-tortoise link to find entrance point of a cycle in a linked list

--- Problems/__get_different_number_cycle_sort.cpp
+++ Problems/__get_different_number_cycle_sort.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 // https://www.pramp.com/challenge/aK6V5GVZ9MSPqvG1vwQp
 // Idea: remember cycle-sort for the inplace solution
+// Plus: link with hare and tortoise algorithm better explained: https://leetcode.com/problems/find-the-duplicate-number/solution/
+// (detection of the only repeated number in O(N), detects the entrance point of a cycle in
+// a linked list, hare twice as fast as the tortoise - see final code, or the idea of bits)
 
 // Given an array arr of unique nonnegative integers, implement a function 
 // getDifferentNumber that finds the smallest nonnegative integer that is NOT in the array.
