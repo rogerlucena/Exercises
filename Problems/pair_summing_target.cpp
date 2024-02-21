7ee346d8218025643fb4f23cab66a4a09620844f
Roger Leite Lucena <27161330+rogerlucena@users.noreply.github.com>
2024-02-21 22:05:49 +0100

    Update pair_summing_target.cpp

--- Problems/pair_summing_target.cpp
+++ Problems/pair_summing_target.cpp
@@ -11,21 +11,19 @@ using namespace std;
 
 */
 
-pair<int, int> FindPairSummingToTarget(const vector<int>& nums, int target){
+pair<int, int> FindPairSummingToTarget(const vector<int>& nums, int target) {
     int index1 = 0;
-    int index2 = nums.size()-1;
+    int index2 = nums.size() - 1;
 
     pair<int, int> p(-1, -1);
 
-    while(index1 < index2) {
+    while (index1 < index2) {
         int sum = nums[index1] + nums[index2];
-        if(sum < target) {
-         index1++;
-        }
-        else if(sum > target) {
-         index2--;
-        }
-        else {
+        if (sum < target) {
+            index1++;
+        } else if (sum > target) {
+            index2--;
+        } else {
             p = {index1, index2};
             break;
         }
@@ -34,13 +32,11 @@ pair<int, int> FindPairSummingToTarget(const vector<int>& nums, int target){
     return p;
 }
 
-
-int main()
-{
+int main() {
     vector<int> nums = {1, 2, 3, 5, 10, 23, 44, 137};
     int target = 13;
 
-    pair<int, int> p = FindPairSummingToTarget(nums, target);
+    pair<int, int> p = FindPairSummingToTarget(nums, target); // expected answer: (2, 4)
     cout << "The indexes of the numbers are: " << p.first << " " << p.second << endl;
 
     return 0;
