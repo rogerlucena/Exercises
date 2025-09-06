c8def75e90296867d491073b353c3b01983a1187
Roger Leite Lucena <rogerleitelucena@gmail.com>
2025-09-06 20:01:46 -0300

    Update __ problem

--- Problems/__next_greater.cpp
+++ Problems/__next_greater.cpp
@@ -6,8 +6,9 @@
 
 using namespace std;
 
-// https://www.interviewbit.com/problems/nextgreater/
-// Idea: use stack to do it in-place, good exemple of exercise using stack
+// https://www.interviewbit.com/problems/nextgreater 
+// https://leetcode.com/problems/next-greater-element-ii (version with circular array below)
+// Remember: use stack to do it in-place, good exemple of exercise using stack. Case with circular array is below.
 
 // Given an array, find the next greater element G[i] for every element A[i] in the array. The Next greater 
 // Element for an element A[i] is the first greater element on the right side of A[i] in array.
@@ -51,6 +52,36 @@ vector<int> nextGreater(vector<int> &A) {
 	}
 
 	return A;
+}
+
+// https://leetcode.com/problems/next-greater-element-ii (version with circular array)
+// Remember: iterate twice over the array to simulate circularity, in the final pass it will let all "next_greater"
+// values updated considering all the elements to the right (all the elements of "nums", as it should be for a circular array).
+
+// Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater 
+// number for every element in nums.
+
+// The next greater number of a number x is the first greater number to its traversing-order next in the array, which 
+// means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+// Example:
+// Input: nums = [1,2,1]
+// Output: [2,-1,2]
+
+vector<int> nextGreaterElements(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> next_greater(n);
+    stack<int> st;
+    for (int i = 2 * n - 1; i >= 0; --i) {  // iterate twice over the array to simulate circularity.
+        int pos = i % n;
+        while (!st.empty() && nums[pos] >= st.top()) {
+            st.pop();
+        }
+        next_greater[pos] = st.empty() ? -1 : st.top();
+        st.push(nums[pos]);
+    }
+
+    return next_greater;
 }
 
 int main() {
