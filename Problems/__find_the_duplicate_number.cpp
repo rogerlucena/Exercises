5ce116858cec118a1d09d4c983e2780ec2a9cd20
Roger Leite Lucena <rogerleitelucena@gmail.com>
2026-07-22 19:45:44 -0300

    Update __ problem

--- Problems/__find_the_duplicate_number.cpp
+++ Problems/__find_the_duplicate_number.cpp
@@ -1,10 +1,14 @@
+#include <cmath>
 #include <iostream>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 // https://leetcode.com/problems/find-the-duplicate-number/ (solution explains hare tortoise visually there)
 // also explained here: https://www.geeksforgeeks.org/dsa/find-any-one-of-the-multiple-repeating-elements-in-read-only-array-set-2/
+// https://neetcode.io/problems/find-duplicate-integer
+// https://www.interviewbit.com/problems/find-duplicate-in-array
 
 // Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 // There is only one repeated number in nums, return this repeated number.
@@ -50,9 +54,33 @@ int findDuplicate(vector<int>& nums) {
 // e/v time after tortoise start from 0 again
 // position of hare will be: (e/v * v) + (v.t-e) == v.t modulus C which is 0 (entrance of the cycle as we wanted).
 
-// Idea for binary search: find the smallest number such that the count of numbers less than or equal to it is greater than the number itself.
-// 		log(N) times it goes through the entire array to get the count above = N log (N) solution
-// clever idea.
+// Other nice/genius idea (in-place, but would have to write on input = use the array itself as a hash map "seen" using the negative sign to flag number already seen):
+// traverse the list for i=0 to n-1 elements {
+//   check for sign of A[abs(A[i])] ;
+//   if positive then
+//      make it negative by   A[abs(A[i])]=-A[abs(A[i])];
+//   else  // i.e., A[abs(A[i])] is negative
+//      this   element (ith element of list) is a repetition
+// }
+// From: https://www.geeksforgeeks.org/find-duplicates-in-on-time-and-constant-extra-space
+
+// Solution changing the input (cycle sort - problem from Pramp).
+// O(N) in time and O(1) in space.
+int findDuplicate(vector<int>& nums) {
+	for (int i = 0; i < nums.size(); ++i) {
+		while (nums[i] != i) {
+			if (nums[nums[i]] == nums[i]) {
+				return nums[i];
+			}
+			swap(nums[i], nums[nums[i]]);
+		}
+	}
+
+	return -1;
+}
+
+// Clever idea for using binary search adaptation here (nice read-only over the input): find the smallest number such that the count of numbers less than or equal to it is greater than the number itself.
+// 		log(N) times it goes through the entire array to get the count above = N log (N) solution.
 int findDuplicateBinarySearch(vector<int>& nums) {
 	// Lambda function to count how many numbers are less than or equal to 'cur'
 	auto small_or_equal = [&](int cur) {
@@ -81,22 +109,75 @@ int findDuplicateBinarySearch(vector<int>& nums) {
 	return duplicate;
 }
 
-// Solution changing the input (cycle sort - problem from Pramp).
-// O(N) in time and O(1) in space.
-int findDuplicate(vector<int>& nums) {
-	for (int i = 0; i < nums.size(); ++i) {
-		while (nums[i] != i) {
-			if (nums[nums[i]] == nums[i]) {
-				return nums[i];
-			}
-			swap(nums[i], nums[nums[i]]);
+// The other solution below is O(sqrt(n)) in space and linear in time - nice idea too (read-only over input).
+// Idea: define intervals of size sqrt(n) and use counting sort to find which interval contains the excess, after that do a final pass holding a hash set "seen" with size at most sqrt(n).
+// Using sqrt for the size "l" of the intervals is a genius trick because it limitates 
+// both the sizes of the vector "intervalsCount" (O(n/l)) and the set "s" later (O(l))
+// ("l" and "n/l" will be both O(sqrt(n)) - final complexity in space :D )
+// Ans: O(sqrt(n)) in memory and only 2 sequential passes through the stream.
+int repeatedNumber(const vector<int> &A) {
+	int n = A.size()-1;
+
+	// Original way: using sqrt
+	int l = ceil(sqrt((double) n)); //  interval size
+	int nBuckets = ceil( ((double) n) / l );
+	int m = n % l;
+
+    // Trying to use log n in space (does not work - size of the set "s" below is not limited)
+	// int nBuckets = floor(log((double) n));
+	// nBuckets = nBuckets == 0 ? 1 : nBuckets;
+	// int l = n / nBuckets; // interval size
+	// int m = l;
+	// if(n % nBuckets != 0) {
+	// 	m = n % nBuckets;
+	// 	nBuckets++;
+	// }
+	
+	// cout << "l: " << l << endl;
+	// cout << "nBuckets: " << nBuckets << endl;
+	// cout << "m: " << m << endl;
+
+	vector<int> intervalsCount (nBuckets, 0); // O(sqrt(n)) in space
+
+	for(int el : A) {
+		++intervalsCount[(el-1)/l];
+	} 
+
+	int indexInterval = 0;
+	for(; indexInterval < nBuckets; indexInterval++) {
+		int count = intervalsCount[indexInterval];
+		if(count > l || (indexInterval == nBuckets-1 && count > m)) {
+			break;
 		}
 	}
+	int intervalDown = indexInterval*l+1;
+	int intervalUp = (indexInterval+1)*l;
+
+	// cout << "intervalDown: " << intervalDown << endl;
+	// cout << "intervalUp: " << intervalUp << endl;
+	
+	// cout << "vector intervalsCount: ";
+	// printVector(intervalsCount);
+
+	unordered_set<int> s = {}; // O(sqrt(n)) in space
+
+	for(int el : A) {
+		if(el < intervalDown || el > intervalUp) {
+			continue;
+		}
+		if(s.find(el) != s.end()) {
+			return el;
+		}
+		s.insert(el);
+	}
+
+	// cout << "s.size(): " << s.size() << endl;
 
 	return -1;
 }
 
-// Bit solution below - O(N log N), O(1) in space. 
+
+// Bit solution below (counting bits in [from 1 to n] and in nums and comparing) - O(N log N), O(1) in space.
 // Algorithm:
 // 1. Initialize the answer (duplicat) to 0
 // 2. Iterate over each bit from 0 to bits where bits is the bit-length of n.
